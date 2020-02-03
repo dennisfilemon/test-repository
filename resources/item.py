f7853221f038c046b6a70765b7f63b5473dce4d0
Dennis Filemon <dennis@justdelegate.co>
2020-02-03 14:00:01 +0700

    testing

--- resources/item.py
+++ resources/item.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
-from random import shuffle
+# from random import shuffle
 import requests
 import json
 
