4faf5daba7c36bc47f70a212e1673e6e4412becc
Dennis Filemon <dennis@justdelegate.co>
2020-02-03 14:26:13 +0700

    testing

--- models/item.py
+++ models/item.py
@@ -4,7 +4,7 @@ from db import db
 class ItemModel(db.Model):
     __tablename__ = 'items'
 
-    # id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     # name = db.Column(db.String(80))
     # price = db.Column(db.Float(precision=2))
     #
