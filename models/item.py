31f37d494881d221a5a8cbeb17de7ad9543f4f5d
Dennis Filemon <dennis@justdelegate.co>
2020-02-03 12:45:23 +0700

    testing save news

--- models/item.py
+++ models/item.py
@@ -5,16 +5,22 @@ class ItemModel(db.Model):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    price = db.Column(db.Float(precision=2))
+    # name = db.Column(db.String(80))
+    # price = db.Column(db.Float(precision=2))
+    #
+    # store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    # store = db.relationship('StoreModel')
+    category = db.Column(db.String(80))
+    country = db.Column(db.String(80))
+    title = db.Column(db.String(120))
+    content = db.Column(db.Text)
 
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-    store = db.relationship('StoreModel')
 
-    def __init__(self, name, price, store_id):
-        self.name = name
-        self.price = price
-        self.store_id = store_id
+    def __init__(self, category, country, title, content):
+        self.category = category
+        self.country = country
+        self.title = title
+        self.content = content
 
     def json(self):
         return {'name': self.name, 'price': self.price}
--- resources/item.py
+++ resources/item.py
@@ -1,20 +1,23 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
+from random import shuffle
+import requests
+import json
 
 
 class Item(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
+    parser.add_argument('country',
+                        type=str,
                         required=True,
                         help="This field cannot be left blank!"
                         )
-    parser.add_argument('store_id',
-                        type=int,
-                        required=True,
-                        help="Every item needs a store_id."
-                        )
+    # parser.add_argument('category',
+    #                     type=str,
+    #                     required=True,
+    #                     help="Please specify the category of the news."
+    #                     )
 
     @jwt_required()
     def get(self, name):
@@ -24,12 +27,32 @@ class Item(Resource):
         return {'message': 'Item not found'}, 404
 
     def post(self, name):
-        if ItemModel.find_by_name(name):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        # if ItemModel.find_by_name(name):
+        #     return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, **data)
+        url = ('https://newsapi.org/v2/top-headlines?'
+               'category=' + name +
+               'country=' + data['country'] +
+               'apiKey=8b6adf725c5746738a10b64ad98c8445')
+
+        response = requests.get(url)
+        json_data = response.text
+        parsed = json.loads(json_data)
+
+        if parsed['status'] != 'ok':
+            return {'message': "An error occurred when retrieving the data."}, 500
+
+        output = parsed['articles']
+        shuffle(output)
+        for k,v in output[0].iteritems():
+            if k == 'title':
+                title = v
+            elif k == 'content':
+                content = v
+
+        item = ItemModel(name, data['country'], title, content)
 
         try:
             item.save_to_db()
